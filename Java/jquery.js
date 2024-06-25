f410a09239c37d286a29fcad3437ad9b063123a7
alexanderoficial <138260223+alexanderoficial@users.noreply.github.com>
2024-06-25 12:49:07 -0500

    Update jquery.js
    AGREGAR EJ JAVA PARA OCULTAR Y MOSTRAR

--- Java/jquery.js
+++ Java/jquery.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+    $('#mas').on('click', function(e) {
+        e.preventDefault();
+        $('#barra-adicional').toggleClass('active');
+    });
+
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('#mas').length && !$(event.target).closest('#barra-adicional').length) {
+            $('#barra-adicional').removeClass('active');
+        }
+    });
+});
 
