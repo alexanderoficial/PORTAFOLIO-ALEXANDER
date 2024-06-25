d456ce5928e5febfbf12a6db2724b3650a0a8229
alexanderoficial <138260223+alexanderoficial@users.noreply.github.com>
2024-06-25 13:03:45 -0500

    Update jquery.js
    MODIFCVACION

--- Java/jquery.js
+++ Java/jquery.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
+    // Toggle de la clase 'active' en #barra-adicional al hacer clic en #mas
     $('#mas').on('click', function(e) {
         e.preventDefault();
         $('#barra-adicional').toggleClass('active');
     });
-    
+
+    // Cerrar #barra-adicional si se hace clic fuera de #mas y #barra-adicional
     $(document).on('click', function(event) {
         if (!$(event.target).closest('#mas').length && !$(event.target).closest('#barra-adicional').length) {
             $('#barra-adicional').removeClass('active');
