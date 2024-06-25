d46f3b6b1bf1519036fc5c7bd80ca156b9c806d4
alexanderoficial <138260223+alexanderoficial@users.noreply.github.com>
2024-06-25 12:52:57 -0500

    Update jquery.js
    SS  AHAHAHAHAHH DD

--- Java/jquery.js
+++ Java/jquery.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
         e.preventDefault();
         $('#barra-adicional').toggleClass('active');
     });
-
     $(document).on('click', function(event) {
         if (!$(event.target).closest('#mas').length && !$(event.target).closest('#barra-adicional').length) {
             $('#barra-adicional').removeClass('active');
